459feec8e0ea62284c1044b41fdef41cb57ec4f6
Amiran Bestaev <AmiranB@MacBook-Pro-Amiran.local>
2022-06-11 19:35:31 +0300

    created markup slider

---
+++ script.js
@@ -0,0 +1,20 @@
+let positon = 0;
+const slideToShow = 1;
+const slideToscroll = 1;
+
+// class Slider
+// {
+// 	constructor(prev, next)
+// 	{
+// 		this.prev = prev;
+// 		this.next = next;
+// 	}
+// 	nextSlide()
+// 	{
+// 		this.next = this.next++;
+// 	}
+// 	prevSlide()
+// 	{
+// 		this.prev = this.prev--;
+// 	}
+// }
