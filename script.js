9cd14953b83286db34b56719a32b211b6f9ac32f
Amiran Bestaev <AmiranB@MacBook-Pro-Amiran.local>
2022-06-13 14:43:40 +0300

    corrected slider loop

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 
 addEventListener('DOMContentLoaded', () =>
 {
-	let position = 0;
 	const slideToShow = 1;
 	const slideToscroll = 1;
 
 	const container = document.querySelector('.main-slider__container');
 	const track = document.querySelector('.main-slider__track');
-	const slide = document.querySelectorAll('.main-slider__slide');
+	const slides = document.querySelectorAll('.main-slider__slide');
 	const btnPrev = document.querySelector('.main-slider__button--prev');
 	const btnNext = document.querySelector('.main-slider__button--next');
 	const slideWidth = container.clientWidth / slideToShow;
 	const movePosition = slideToscroll * slideWidth;
-	const totalSlides = slide.length * slideWidth;
+	const totalSlides = slides.length * slideWidth;
+	let currentIndexSlide = 0;
 
-
-	console.log(position);
-	slide.forEach((item) =>
+	slides.forEach((item) =>
 	{
 		item.style.minWidth = slideWidth;
 	});
 	btnNext.addEventListener('click',() =>
 	{
-		position -= movePosition;
-		console.log(position);
-		setPosition();
+		if(currentIndexSlide === slides.length - 1)
+		{
+			currentIndexSlide = - 1;
+		}
+		else
+		{
+			currentIndexSlide++;
+			setPosition(currentIndexSlide);
+		}
 	});
 	btnPrev.addEventListener('click',() =>
 	{
-		position += movePosition;
-		console.log(position);
-		setPosition();
-		restartSlider();
+		if(currentIndexSlide - 1 === -1)
+		{
+			currentIndexSlide = slides.length;
+		}
+		else
+		{
+			currentIndexSlide--;
+			setPosition(currentIndexSlide);
+		}
 	});
-	function setPosition()
+	function setPosition(index)
 	{
+		let position = 0;
+		position = (index * slideWidth) * -1;
 		track.style.transform = `translateX(${position}px)`;
-	}
-	function restartSlider()
-	{
-		if(position > 0)
-		{
-			track.style.transform = `translateX(${position = 0}px)`;
-		}
-		else if(position === -totalSlides)
-		{
-			track.style.transform = `translateX(${position = 0}px)`;
-		}
 	}
 });
