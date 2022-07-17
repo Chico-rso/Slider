ef58eb0556d67548009c7517ca65b43386351153
Amiran Bestaev <AmiranB@MacBook-Pro-Amiran.local>
2022-07-17 13:19:30 +0300

    complete slider v2.0

--- script.js
+++ script.js
@@ -4,26 +4,63 @@ let btnNext = document.querySelector('.slider__btn-next');
 let slides = document.querySelectorAll('.slide');
 let sliderItemWidth = slides[0].offsetWidth;
 let trackWidth = sliderWrap.offsetWidth * slides.length;
+let counterSlide = 0;
+
+btnNext.addEventListener('click', nextSlide);
+btnPrev.addEventListener('click', prevSlide);
 
 
-let currentSlide = 0;
-console.log(slides);
-
-
-
-btnNext.addEventListener('click', function() {
-	console.log(currentSlide);
-	currentSlide++;
-	if (currentSlide >= slides.length - 1) {
-		currentSlide = -1;
+function nextSlide()
+{
+	if(counterSlide < slides.length - 1)
+	{
+		counterSlide++;
 	}
-	sliderWrap.style.transform = 'translateX(' + (-currentSlide * sliderItemWidth) + 'px)';
-});
-btnPrev.addEventListener('click', function() {
-	currentSlide--;
-	console.log(currentSlide);
-	if (currentSlide <= -slides.length + 1) {
-		currentSlide = slides.length - 1;
+	else
+	{
+		counterSlide = 0;
 	}
-	sliderWrap.style.transform = 'translateX(' + (-currentSlide * sliderItemWidth) + 'px)';
-});
+	sliderWrap.style.transform = 'translateX(' + (-counterSlide * sliderItemWidth) + 'px)';
+	dotsActive();
+}
+
+function prevSlide()
+{
+	if(counterSlide > 0)
+	{
+		counterSlide--;
+	}
+	else
+	{
+		counterSlide = slides.length - 1;
+	}
+	sliderWrap.style.transform = 'translateX(' + (-counterSlide * sliderItemWidth) + 'px)';
+	dotsActive();
+}
+
+
+function dotsActive()
+{
+	let dots = document.querySelectorAll('.dot');
+	for(let i = 0; i < dots.length; i++)
+	{
+		dots[i].classList.remove('active');
+	}
+	dots[counterSlide].classList.add('active');
+}
+dotsActive();
+
+function dotsClick()
+{
+	let dots = document.querySelectorAll('.dot');
+	for(let i = 0; i < dots.length; i++)
+	{
+		dots[i].addEventListener('click', function()
+		{
+			counterSlide = i;
+			sliderWrap.style.transform = 'translateX(' + (-counterSlide * sliderItemWidth) + 'px)';
+			dotsActive();
+		});
+	}
+};
+dotsClick();
